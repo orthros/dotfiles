626c32ca7a0b2646d9d0907d3ec1240fe0f9cd41
orthros <colinnlsn@gmail.com>
2017-11-16 06:55:12 -0600

    Updated the configure.sh with my personal dotfiles Now assuming configure.sh is in the cloned dotfiles git repository as opposed to curl'd Not sure if this is the right long term solution, but going for it for now

--- configure.sh
+++ configure.sh
@@ -83,24 +83,23 @@ mv dircolors.256dark .dircolors
 
 # Pull down personal dotfiles
 echo ''
-read -p "Do you want to use jldeen's dotfiles? y/n" -n 1 -r
+read -p "Do you want to configure the dotfiles? y/n" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    echo ''
-	echo "Now pulling down jldeen dotfiles..."
-	git clone https://github.com/jldeen/dotfiles.git ~/.dotfiles
-	echo ''
-	cd $HOME/.dotfiles && echo "switched to .dotfiles dir..."
-	echo ''
-	echo "Checking out WSL branch..." && git checkout wsl
-	echo ''
-	echo "Now configuring symlinks..." && $HOME/.dotfiles/script/bootstrap
-    if [[ $? -eq 0 ]]
+    if [ ! -f ./script/bootstrap ]
     then
-        echo "Successfully configured your environment with jldeen's dotfiles..."
+        echo 'Could not find ./script/bootstrap.... did you clone this out of the repository?'
+    fi
     else
-        echo "jldeen's dotfiles were not applied successfully..." >&2
+        echo ''
+        echo "Now configuring symlinks..." && ./script/bootstrap
+        if [[ $? -eq 0 ]]
+        then
+            echo "Successfully configured your environment with dotfiles..."
+        else
+            echo "Dotfiles were not applied successfully..." >&2
+    fi
 fi
 else 
 	echo ''
@@ -113,7 +112,6 @@ else
 	echo "source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc && echo "added zsh-autosuggestions to .zshrc..."
 	echo ''
 	echo "source $HOME/.git-completion.bash" >> ${ZDOTDIR:-$HOME}/.bashrc && echo "added git-completion to .bashrc..."
-	
 fi
 
 # Set default shell to zsh
