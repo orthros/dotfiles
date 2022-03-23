feb6e4e1691e8cbcf7416911ee57895259e45d30
Colin Nelson <colin@orthros.dev>
2022-03-23 10:20:08 -0700

    feat(zsh): add utilities file for git

---
+++ config/zsh/git-utils.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env zsh
+
+if ! [ -z "${_GIT_UTILS_INCLUDE+x}" ]; then
+	return 0
+fi
+
+export _GIT_UTILS_INCLUDE=true
+
+# Git Helper
+function in_git_dir() {
+  git rev-parse --git-dir &>/dev/null
+  # If non zero we have an error code...
+  if (( $? )); then
+    return 1
+  fi
+  return 0
+}
+
+function get_origin_last_path() {
+    echo $(git remote -v | rg '^origin\s+.+/([\w-]+)\s+\(fetch\)$' -r '$1' --only-matching)
+}
+
+function get_tmux_session_name() {
+	local branch_name=$(git branch --no-color --show-current)
+	local origin_path=$(get_origin_last_path)
+	local session_name="Git-${origin_path}-${branch_name}"
+	echo ${session_name}
+}
