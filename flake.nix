953f58425a7f251f69e5b63e7bc1582dbc51852d
Colin Nelson <colin@orthros.dev>
2026-08-06 09:04:01 -0700

    [nix] add orthros/.vim flake input and homeManagerModule
    -Add github:orthros/.vim as flake input (following nixpkgs and home-manager).
    - Import vim-config.homeManagerModules.default into default homeConfiguration in flake.nix.
    - Lock orthros/.vim dependency in flake.lock.

--- flake.nix
+++ flake.nix
@@ -7,15 +7,21 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    vim-config = {
+      url = "github:orthros/.vim";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, vim-config, ... }:
     let
       mkHome = { username, homeDirectory, system ? "x86_64-linux", stateVersion ? "24.05" }:
         home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.${system};
           modules = [
             ./home.nix
+            vim-config.homeManagerModules.default
             {
               home = {
                 inherit username homeDirectory stateVersion;
